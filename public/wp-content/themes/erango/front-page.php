6daac094b7432a1dab6bfdfdfc35b26a51e32583
Cameron Drysdale <cameron@driz.co.uk>
2021-03-21 13:47:42 +0000

    updates

--- public/wp-content/themes/erango/front-page.php
+++ public/wp-content/themes/erango/front-page.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="columns is-align-center">
                 <div class="col-5">
-                    <h1 class="color-orange margin-b-48" data-aos="fade-right">Managing Competencies in a Complex business world.</h1>
+                    <h1 class="color-orange margin-b-48" data-aos="fade-right">A simple management of competencies in a complex business world.</h1>
                     <div class="" data-aos="fade-right">
                         <a class="down" href="#section2">
                             <i class="far fa-arrow-down"></i>
--- public/wp-content/themes/erango/page-features.php
+++ public/wp-content/themes/erango/page-features.php
@@ -14,8 +14,12 @@
 <?php endif; ?>
 <section class="section section--off-white">
   <div class="container">
+    <div class="columns">
+      <div class="col-12">
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, cupiditate debitis laboriosam reiciendis rem quas error perspiciatis eos obcaecati fuga distinctio blanditiis eius doloribus? Rerum in praesentium sint nesciunt? Voluptatibus.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi accusantium incidunt ex alias optio porro tempore harum in quo similique, qui odio ipsam quia fugit iusto, molestias quis, adipisci praesentium.</p>
+    </div>
+    </div>
     </div>
 </section>
 <section class="section section--white section--full-height">
@@ -41,11 +45,33 @@
     </div>
   </div>
 </section>
-<!-- <section class="section section--white">
+<section class="section section--white">
       <div class="container">
-
+        <h2 class="text-center margin-b-48">Section title?</h2>
+      <div class="columns is-align-center margin-b-48">
+            <div class="col-5">
+                  <h3>Header</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rerum, numquam asperiores commodi adipisci necessitatibus dolorum voluptatum nisi? Sequi, maiores unde aperiam nisi dolorem minus vero maxime consectetur non labore!</p>
+            </div>
+            <div class="col-6 offset-1">
+                <figure class="">
+                <img class="rounded" src="https://picsum.photos/1920/1080?random=5" width="1920" height="1080">
+                </figure>
+            </div>
+        </div>
+        <div class="columns is-align-center">
+            <div class="col-6">
+                <figure class="">
+                <img class="rounded" src="https://picsum.photos/1920/1080?random=6" width="1920" height="1080">
+                </figure>
+            </div>
+            <div class="col-5 offset-1">
+                <h3>Header</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rerum, numquam asperiores commodi adipisci necessitatibus dolorum voluptatum nisi? Sequi, maiores unde aperiam nisi dolorem minus vero maxime consectetur non labore!</p>
+                </div>
+        </div>
       </div>
-</section> -->
+</section>
 <?php endwhile; ?>
 
 <?php else : ?>
